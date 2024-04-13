6c0eb9ec9c723ae38d093b9f54dcc099cf7afe13
jferdin4nd <165064536+jferdin4nd@users.noreply.github.com>
2024-04-12 18:26:38 -0600

    Update jferdin4nd.sh

--- jferdin4nd.sh
+++ jferdin4nd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-FILENAME=$(date +"%m%d%H%M")
+FILENAME=$(date +"%m%d%H:%M")
 cd ~/jferdin4nd
-echo "$(git pull)$FILENAME" >> ~/jferdin4nd.log
+echo "$FILENAME $(git pull)" >> ~/jferdin4nd.log
 
