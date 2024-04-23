c6ef4d3100e50d0ac9c0af89e26a41194b67e839
jferdin4nd <165064536+jferdin4nd@users.noreply.github.com>
2024-04-23 15:34:02 -0600

    listo

--- scripts/iso.sh
+++ scripts/iso.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-#Colours
-green="\e[0;32m\033[1m"
-end="\033[0m\e[0m"
-red="\e[0;31m\033[1m"
-blue="\e[0;34m\033[1m"
-yellow="\e[0;33m\033[1m"
-purple="\e[0;35m\033[1m"
-cyan="\e[0;36m\033[1m"
-gray="\e[0;37m\033[1m"
-
 d_file=~/download_iso
 downloads=~/Downloads/
 storage=~/ISO/
@@ -30,12 +20,8 @@ function mvFiles(){
 }
 
 function downloadISO(){
-    cd $downloads
-    echo -e "${red}Downloading ISOs......${cyan}"
-    wget -b -i ~/d_iso.txt
-    echo -e "${end}End"
 
+    wget -f 
 }
 mkFolders
-downloadISO
 mvFiles
